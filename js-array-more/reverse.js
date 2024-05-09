7003d4300e1fac9990730d248232bbb8652c5ffc
subas-roy <subasroy46@gmail.com>
2024-05-10 02:10:04 +0600

    array reversing practice

---
+++ js-array-more/reverse.js
@@ -0,0 +1,28 @@
+let numbers = [1,2,3,4,5,6,7];
+// console.log(numbers)
+// console.log(numbers.reverse())
+
+
+// rev_nums = []
+// for(num of numbers){
+//     rev_nums.unshift(num)
+// }
+// console.log(rev_nums)
+
+
+// for(i = 0; i < numbers.length; i++){
+//     console.log(numbers[i])
+// }
+
+
+const reversed = []
+for(i = numbers.length -1; i >= 0; i--){
+    // console.log(numbers[i])
+    reversed.push(numbers[i])
+}
+console.log(reversed)
+
+// push_nums(){
+    
+// }
+
