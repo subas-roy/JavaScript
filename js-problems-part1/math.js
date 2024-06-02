a25af59c46fdf5b39bd9cf6aef1367df50eddc4f
subas-roy <subasroy46@gmail.com>
2024-06-02 19:21:22 +0600

    built in math functions

---
+++ js-problems-part1/math.js
@@ -0,0 +1,30 @@
+/**
+ * Math
+ * 
+ */
+const min = Math.min(100,55,99)
+const max = Math.max(100,55,99)
+const pi = Math.PI
+const abs = Math.abs(-88)
+
+const round = Math.round(50.4)
+const ceil = Math.ceil(50.4)
+const floor = Math.floor(50.9)
+
+const random = Math.random() * 10
+
+console.log(min)
+console.log(max)
+console.log(pi)
+console.log(abs)
+
+console.log(round)
+console.log(ceil)
+console.log(floor)
+
+console.log(random)
+console.log(Math.round(random)) // int
+
+
+
+
