46775cef5fcd460b01e3c0638b1ebeebdf223de6
subas-roy <subasroy46@gmail.com>
2024-08-11 08:57:33 +0600

    double equal (==) vs triple equal (===), implicit conversion

---
+++ common-js-concepts/triple.js
@@ -0,0 +1,11 @@
+// always use === 
+// equal comparision does not work of non-premitive
+
+const frist = [45,54,98]
+const second = [45,54,90]
+if(frist, second){
+  console.log('values are equal')
+}
+else{
+  console.log('values are not equal')
+}
