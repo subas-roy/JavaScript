59cc9e3a38642988a4dfbaa9fbbe6ecdf3b6b586
subas-roy <subasroy46@gmail.com>
2024-08-14 15:48:57 +0600

    async await

---
+++ explore-api/js/comments.js
@@ -0,0 +1,18 @@
+const loadComments = () => {
+  fetch('https://jsonplaceholder.typicode.com/comments')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log(error))
+}
+
+
+const loadComments2 = async() => {
+  try {
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments');
+    const data = await res.json();
+    console.log(data);
+  }
+  catch(error) {
+    console.error('data load error!')
+  }
+}
