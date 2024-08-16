8680cf28c6c31b4c1ff522445fd0f7639ff49046
subas-roy <subasroy46@gmail.com>
2024-08-16 20:51:47 +0600

    Single-threaded, Asynchronous vs Synchronous

---
+++ more-js-concepts/async.js
@@ -0,0 +1,12 @@
+console.log(1)
+console.log(2)
+setTimeout(() => {
+  console.log('lazy logged')
+}, 3000)
+console.log(4)
+console.log(5)
+console.log(6)
+
+function doSomething(){
+  console.log(3)
+}
---
+++ more-js-concepts/context.js
@@ -0,0 +1,8 @@
+const a = 5;
+function add(num1,num2){
+  const result = num1+num2+a;
+  return result;
+}
+
+console.log(add(5,10))
+// console.log(result)
